ca139d72518a6ba3d41632ab1c7d4b6cdb607dfd
Build <jwbradley2003@gmail.com>
2022-04-26 21:34:56 -0400

    bonk

---
+++ CustomDamage/DamageTypeConfig.cs
@@ -0,0 +1,58 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="DamageTypeConfig.cs" company="Build">
+// Copyright (c) Build. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace CustomDamage
+{
+    using CustomDamage.Models;
+
+    /// <summary>
+    /// Handles the <see cref="HitboxValue"/>s for each armor type.
+    /// </summary>
+    public class DamageTypeConfig
+    {
+        /// <summary>
+        /// Gets or sets the damage values for unarmored targets.
+        /// </summary>
+        public HitboxValue UnArmoured { get; set; } = new();
+
+        /// <summary>
+        /// Gets or sets the damage values for targets with light armor.
+        /// </summary>
+        public HitboxValue LightArmour { get; set; } = new();
+
+        /// <summary>
+        /// Gets or sets the damage values for targets with combat armor.
+        /// </summary>
+        public HitboxValue CombatArmour { get; set; } = new();
+
+        /// <summary>
+        /// Gets or sets the damage values for targets with heavy armor.
+        /// </summary>
+        public HitboxValue HeavyArmour { get; set; } = new();
+
+        /// <summary>
+        /// Gets the damage value for the corresponding armor type and hitbox.
+        /// </summary>
+        /// <param name="armor">The armor type.</param>
+        /// <param name="hitbox">The hitbox.</param>
+        /// <returns>The damage value to use.</returns>
+        public float GetValue(ItemType armor, HitboxType hitbox)
+        {
+            switch (armor)
+            {
+                case ItemType.ArmorLight:
+                    return LightArmour.GetValue(hitbox);
+                case ItemType.ArmorCombat:
+                    return CombatArmour.GetValue(hitbox);
+                case ItemType.ArmorHeavy:
+                    return HeavyArmour.GetValue(hitbox);
+                default:
+                    return UnArmoured.GetValue(hitbox);
+            }
+        }
+    }
+}
---
+++ CustomDamage/EventHandlers.cs
@@ -0,0 +1,61 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="EventHandlers.cs" company="Build">
+// Copyright (c) Build. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace CustomDamage
+{
+    using System.Linq;
+    using Exiled.API.Enums;
+    using Exiled.API.Extensions;
+    using Exiled.API.Features.Items;
+    using Exiled.Events.EventArgs;
+
+    /// <summary>
+    /// Handles events derived from <see cref="Exiled.Events.Handlers"/>.
+    /// </summary>
+    public class EventHandlers
+    {
+        private readonly Plugin plugin;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EventHandlers"/> class.
+        /// </summary>
+        /// <param name="plugin">An instance of the <see cref="Plugin"/> class.</param>
+        public EventHandlers(Plugin plugin) => this.plugin = plugin;
+
+        /// <inheritdoc cref="Exiled.Events.Handlers.Player.OnShot(ShotEventArgs)"/>
+        public void OnShot(ShotEventArgs ev)
+        {
+            if (ev.Hitbox == null || ev.Shooter == null || ev.Target == null)
+                return;
+
+            if (ev.Shooter.CurrentItem is not Firearm firearm)
+                return;
+
+            if (!DamageTypeExtensions.ItemConversion.TryGetValue(firearm.Type, out DamageType value) ||
+                !plugin.Config.DamageValues.TryGetValue(value, out DamageTypeConfig damageValue))
+                return;
+
+            float damage = GetDamage(damageValue, ev.Hitbox._dmgMultiplier, ev.Target.Items.FirstOrDefault(item => item.Type.IsArmor()));
+            if (damage > 0)
+            {
+                ev.CanHurt = false;
+                ev.Target.Hurt(ev.Shooter, damage, value);
+            }
+        }
+
+        /// <inheritdoc cref="Exiled.Events.Handlers.Server.OnReloadedConfigs()"/>
+        public void OnReloadedConfigs() => plugin.Config.Reload();
+
+        private static float GetDamage(DamageTypeConfig damageTypeConfig, HitboxType hitboxType, Item armor)
+        {
+            if (armor is null)
+                return damageTypeConfig.UnArmoured.GetValue(hitboxType);
+
+            return damageTypeConfig.GetValue(armor.Type, hitboxType);
+        }
+    }
+}
